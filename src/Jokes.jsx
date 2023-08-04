040f6a12e2cf35e1adee2bb6866c41a29a4a7ef2
Rishi <Rishi15599@gmail.com>
2023-08-04 13:28:00 +0530

    changes pushed

---
+++ src/Jokes.jsx
@@ -0,0 +1,60 @@
+import { Button } from "react-bootstrap";
+import { useState } from "react"
+import Popup from "reactjs-popup";
+import React from "react";
+import 'reactjs-popup/dist/index.css'
+import './Jokes.css'
+import CloseButton from 'react-bootstrap/CloseButton';
+
+
+const url = `https://api.chucknorris.io/jokes/random?category=`
+
+const Card = ({ category }) => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [jokes, setJokes] = useState();
+
+  const handleClick = () => {
+    fetch(`${url}${category}`).then(d => d.json()).then(res => {
+      setJokes(res.value)
+      setIsPopupOpen(true);
+    })
+  };
+
+  const handleClose = () => {
+    setIsPopupOpen(false);
+  };
+
+
+  return (
+    <div className="card" onClick={handleClick}>
+      <div className='card-text'>{category}</div> 
+      <div className='card-para'>Unlimited Jokes On {category}</div>
+      <Popup open={isPopupOpen} onClose={handleClose}>
+        <div className="popup">
+          <span className="popup-header">{category}</span><span className="close-button"><CloseButton onClick={handleClose} /></span>
+          <div className="popup-jokes-card">
+            <div className="popup-jokes">"{jokes}"</div>
+            {<Button className="popup-button btn-primary" onClick={handleClick} variant="next">Next Jokes</Button>}
+          </div>
+        </div>
+      </Popup>
+    </div>
+  )
+};
+
+const Jokes = () => {
+  const categories = ["animal", "career", "celebrity", "dev", "explicit", "fashion", "food", "history", "money", "movie", "music", "political", "religion", "science", "sport", "travel"]
+
+  return (
+    <div className="main-div">
+      <h1 className="header-content">Chuck Norries</h1>
+      <div className='main-card-jokes'>
+      {categories.map((category) => (
+        <Card category={category} onClick={() => { }} />
+      ))}
+      </div>
+    </div>
+  )
+}
+
+export default Jokes;
